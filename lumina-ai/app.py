8c716be5a17e122224231b2e5858a3dd7d4a1cf7
Krishna18997 <kkhare476@gmail.com>
2026-06-21 00:42:23 +0530

    Update app.py

--- lumina-ai/app.py
+++ lumina-ai/app.py
@@ -4,10 +4,7 @@ from database.auth import create_user, login_user
 from database.stats import get_stats, add_xp
 
 # Database initialize
-init_db()
-from database.auth import create_user, login_user
-from utils.gemini_client import get_gemini_response
-from database.stats import add_xp, get_stats
+
 
 
 # ----------------------------
